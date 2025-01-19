51b1b7c3a642ea20f4fd2187a805555f000d3735
Francisco <github.backstage538@passmail.net>
2025-01-19 21:09:07 +0100

    Update styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   devServer: {
     port: 3110,
@@ -11,7 +12,11 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com' , crossorigin: 'anonymous'},
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous',
+        },
         {
           href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap',
           rel: 'stylesheet',
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#222222',
+        },
+        airbnb: {
+          gray: {
+            light: '#767676',
+            dark: '#484848',
+          },
+          accent: '#FF5A5F',
+          green: '#00A699',
+          orange: '#FC642D',
+        },
+      },
+      fontFamily: {
+        text: ['Nunito', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
