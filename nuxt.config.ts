607c72c0eb87aa1f9f29928bfcc9550611cac641
Francisco <github.backstage538@passmail.net>
2025-01-19 21:06:17 +0100

    Add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,15 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Airbnb | Vacation rentals, cabins, beach houses, & more',
-      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' , crossorigin: 'anonymous'},
+        {
+          href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap',
+          rel: 'stylesheet',
+        },
+      ],
     },
   },
   modules: ['@nuxtjs/tailwindcss'],
