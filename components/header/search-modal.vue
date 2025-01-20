01bb18a55d2bc4a1d8e0c7efca2951fbcd1a5a80
Francisco <github.backstage538@passmail.net>
2025-01-20 20:02:24 +0100

    WIP

--- components/header/search-modal.vue
+++ components/header/search-modal.vue
@@ -21,84 +21,19 @@
       </div>
     </div>
 
-    <div class="flex-grow px-3">
-      <!-- <WhereTrip /> -->
-
-      <div class="search-card">
-        <div class="text-xl font-extrabold">When's your trip?</div>
-        <!-- Tab selector -->
-        <div
-          class="grid items-center grid-cols-3 p-1 my-4 text-sm font-bold text-center bg-gray-200 rounded-full"
-        >
-          <div
-            class="rounded-full border border-[#dddddd] bg-white py-1.5 text-center text-sm"
-          >
-            Dates
-          </div>
-          <div class="text-center">Months</div>
-          <div class="text-center">Flexible</div>
-        </div>
-
-        <!-- Tabs -->
-        <div>
-          <!-- Dates -->
-          <!-- <div></div> -->
-
-          <!-- Months -->
-          <!-- <div></div> -->
-
-          <!-- Flexible -->
-          <div class="">
-            <div class="pt-3 border-t border-gray-200">
-              <div class="mb-2 font-semibold">Stay for a {{ stayForA }} ?</div>
-              <div class="flex items-center justify-between gap-4 text-sm">
-                <button
-                  class="selected-stay"
-                  :class="{ active: stayForA === 'weekend' }"
-                  :disabled="stayForA === 'weekend'"
-                  @click="stayForA = 'weekend'"
-                >
-                  Weekend
-                </button>
-                <button
-                  class="selected-stay"
-                  :class="{ active: stayForA === 'week' }"
-                  :disabled="stayForA === 'week'"
-                  @click="stayForA = 'week'"
-                >
-                  Week
-                </button>
-                <button
-                  class="selected-stay"
-                  :class="{ active: stayForA === 'month' }"
-                  :disabled="stayForA === 'month'"
-                  @click="stayForA = 'month'"
-                >
-                  Month
-                </button>
-              </div>
-            </div>
-            <div class="pt-3 mt-3 border-t border-gray-200">
-              <div class="mb-2 font-semibold">Go anytime</div>
-              <div
-                class="flex gap-2 px-6 -mx-6 overflow-x-scroll no-scrollbar flex-nowrap"
-              >
-                <button
-                  v-for="month of months"
-                  :key="month"
-                  class="w-[114px] flex-none rounded-2xl border border-gray-300 py-2 text-center"
-                >
-                  <Calendar />
-                  <div class="mt-1 text-sm font-medium">
-                    {{ month }}
-                    <div>2025</div>
-                  </div>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div class="flex flex-col flex-grow gap-2 px-3">
+      <details>
+        <summary>
+          <div class="search-card__collapse">Where to?</div>
+        </summary>
+        <WhereTrip />
+      </details>
+      <details>
+        <summary>
+          <div class="search-card__collapse">When's your trip?</div>
+        </summary>
+        <WhenTrip />
+      </details>
     </div>
 
     <div class="flex items-center justify-between px-6 py-2 bg-white">
@@ -117,33 +52,21 @@
 import MagnifyingGlass from '@/components/icons/magnifying-glass.vue';
 import XMark from '@/components/icons/x-mark.vue';
 import WhereTrip from '@/components/search-modal/where-trip.vue';
-import Calendar from '@/components/icons/calendar.vue';
+import WhenTrip from '@/components/search-modal/when-trip.vue';
+
 defineEmits(['close']);
-
-const stayForA = ref<string>('...');
-
-const months = [
-  'January',
-  'Febraury',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
 </script>
 
-<style lang="postcss">
-.selected-stay {
-  @apply rounded-full border border-gray-300 px-3 py-1.5;
+<style>
+details > summary {
+  list-style: none;
+}
 
-  &.active {
-    @apply border-2 border-gray-900 bg-gray-200;
-  }
+details[open] > summary {
+  display: none;
+}
+
+details > summary::-webkit-details-marker {
+  display: none;
 }
 </style>
---
+++ components/search-modal/when-trip.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="search-card">
+    <div class="text-xl font-extrabold">When's your trip?</div>
+    <!-- Tab selector -->
+    <div
+      class="grid items-center grid-cols-3 p-1 my-4 text-sm font-bold text-center bg-gray-200 rounded-full"
+    >
+      <div
+        class="rounded-full border border-[#dddddd] bg-white py-1.5 text-center text-sm"
+      >
+        Dates
+      </div>
+      <div class="text-center">Months</div>
+      <div class="text-center">Flexible</div>
+    </div>
+
+    <!-- Tabs -->
+    <div>
+      <!-- Dates -->
+      <!-- <div></div> -->
+
+      <!-- Months -->
+      <!-- <div></div> -->
+
+      <!-- Flexible -->
+      <div class="">
+        <div class="pt-3 border-t border-gray-200">
+          <div class="mb-2 font-semibold">Stay for a {{ stayForA }} ?</div>
+          <div
+            class="grid items-center justify-between grid-cols-3 gap-4 text-sm"
+          >
+            <button
+              class="selected-stay"
+              :class="{ active: stayForA === 'weekend' }"
+              :disabled="stayForA === 'weekend'"
+              @click="stayForA = 'weekend'"
+            >
+              Weekend
+            </button>
+            <button
+              class="selected-stay"
+              :class="{ active: stayForA === 'week' }"
+              :disabled="stayForA === 'week'"
+              @click="stayForA = 'week'"
+            >
+              Week
+            </button>
+            <button
+              class="selected-stay"
+              :class="{ active: stayForA === 'month' }"
+              :disabled="stayForA === 'month'"
+              @click="stayForA = 'month'"
+            >
+              Month
+            </button>
+          </div>
+        </div>
+        <div class="pt-3 mt-3 border-t border-gray-200">
+          <div class="mb-2 font-semibold">Go anytime</div>
+          <div
+            class="flex gap-2 px-6 -mx-6 overflow-x-scroll no-scrollbar flex-nowrap"
+          >
+            <button
+              v-for="month of months"
+              :key="month"
+              class="w-[114px] flex-none rounded-2xl border border-gray-300 py-2 text-center"
+            >
+              <Calendar />
+              <div class="mt-1 text-sm font-medium">
+                {{ month }}
+                <div>2025</div>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Calendar from '@/components/icons/calendar.vue';
+const stayForA = ref<string>('...');
+
+const months = [
+  'January',
+  'Febraury',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+</script>
+
+<style lang="postcss">
+.selected-stay {
+  @apply rounded-full border border-gray-300 px-3 py-1.5;
+
+  &.active {
+    @apply border-2 border-gray-900 bg-gray-200;
+  }
+}
+</style>
