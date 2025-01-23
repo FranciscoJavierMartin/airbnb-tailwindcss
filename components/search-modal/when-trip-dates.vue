a4f5c80a9bf0aef973f0ce79f7ddf3cd58d2bd0b
Francisco <github.backstage538@passmail.net>
2025-01-23 22:48:35 +0100

    WIP

--- components/search-modal/when-trip-dates.vue
+++ components/search-modal/when-trip-dates.vue
@@ -255,11 +255,11 @@ CIRCULAR
   --gradient-start: 0deg;
   --gradient-end: 0deg;
 
-  box-shadow:
+  /* box-shadow:
     0 -5px 5px rgba(0, 0, 0, 0.2),
     0 5px 5px #ffffff,
     inset 0 -20px 30px #ffffffe5,
-    inset 0 10px 20px rgba(0, 0, 0, 0.2);
+    inset 0 10px 20px rgba(0, 0, 0, 0.2); */
 
   background-image: conic-gradient(
     var(--track-bgc) 0,
@@ -295,8 +295,8 @@ CIRCULAR
       0 -5px 5px rgba(255, 255, 255, 0.4),
       inset 0 5px 3px rgba(255, 255, 255, 0.8),
       inset 0 -5px 5px rgba(0, 0, 0, 0.2),
-      0 11px 18px rgba(0, 0, 0, 0.18),
-      0 -20px 30px rgba(255, 255, 255, 0.8);
+      0 11px 18px rgba(0, 0, 0, 0.18);
+      /* 0 -20px 30px rgba(255, 255, 255, 0.8); */
   }
 
   &::after {
