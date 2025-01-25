2f466473c2c690987fdac59844324d9f1bf5e325
Francisco <github.backstage538@passmail.net>
2025-01-25 10:12:27 +0100

    WIP

--- components/search-modal/when-trip-dates.vue
+++ components/search-modal/when-trip-dates.vue
@@ -1,3 +1,193 @@
 <template>
-  <div class=""></div>
+  <div class="relative">
+    <ul class="-mx-6 pb-2 text-center text-xs font-semibold text-[#717171]">
+      <li class="inline-block w-[42px]">Mo</li>
+      <li class="inline-block w-[42px]">Tu</li>
+      <li class="inline-block w-[42px]">We</li>
+      <li class="inline-block w-[42px]">Th</li>
+      <li class="inline-block w-[42px]">Fr</li>
+      <li class="inline-block w-[42px]">Sa</li>
+      <li class="inline-block w-[42px]">Su</li>
+    </ul>
+  </div>
+  <hr class="-mx-6" />
+  <div class="h-[400px] overflow-y-scroll no-scrollbar">
+    <div>
+      <div class="py-4">
+        <div class="font-semibold">January 2025</div>
+        <table class="search-dates-table">
+          <tbody>
+            <tr>
+              <td><div class="date">&nbsp;</div></td>
+              <td><div class="date">&nbsp;</div></td>
+              <td><div class="date date-disabled">1</div></td>
+              <td><div class="date date-disabled">2</div></td>
+              <td><div class="date date-disabled">3</div></td>
+              <td><div class="date date-disabled">4</div></td>
+              <td><div class="date date-disabled">5</div></td>
+            </tr>
+            <tr>
+              <td><div class="date date-disabled">6</div></td>
+              <td><div class="date date-disabled">7</div></td>
+              <td><div class="date date-disabled">8</div></td>
+              <td><div class="date date-disabled">9</div></td>
+              <td><div class="date date-disabled">10</div></td>
+              <td><div class="date date-disabled">11</div></td>
+              <td><div class="date date-disabled">12</div></td>
+            </tr>
+            <tr>
+              <td><div class="date date-disabled">13</div></td>
+              <td><div class="date date-disabled">14</div></td>
+              <td><div class="date date-disabled">15</div></td>
+              <td><div class="date">16</div></td>
+              <td><div class="date">17</div></td>
+              <td><div class="date">18</div></td>
+              <td><div class="date">19</div></td>
+            </tr>
+            <tr>
+              <td><div class="date">20</div></td>
+              <td><div class="date">21</div></td>
+              <td><div class="date">22</div></td>
+              <td><div class="date">23</div></td>
+              <td><div class="date">24</div></td>
+              <td><div class="date">25</div></td>
+              <td><div class="date">26</div></td>
+            </tr>
+            <tr>
+              <td><div class="date">27</div></td>
+              <td><div class="date">28</div></td>
+              <td><div class="date">29</div></td>
+              <td><div class="date">30</div></td>
+              <td><div class="date">31</div></td>
+              <td><div class="date">&nbsp;</div></td>
+              <td><div class="date">&nbsp;</div></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="py-4">
+        <div class="font-semibold">January 2025</div>
+        <table class="search-dates-table">
+          <tbody>
+            <tr>
+              <td><div class="date">&nbsp;</div></td>
+              <td><div class="date">&nbsp;</div></td>
+              <td><div class="date date-disabled">1</div></td>
+              <td><div class="date date-disabled">2</div></td>
+              <td><div class="date date-disabled">3</div></td>
+              <td><div class="date date-disabled">4</div></td>
+              <td><div class="date date-disabled">5</div></td>
+            </tr>
+            <tr>
+              <td><div class="date date-disabled">6</div></td>
+              <td><div class="date date-disabled">7</div></td>
+              <td><div class="date date-disabled">8</div></td>
+              <td><div class="date date-disabled">9</div></td>
+              <td><div class="date date-disabled">10</div></td>
+              <td><div class="date date-disabled">11</div></td>
+              <td><div class="date date-disabled">12</div></td>
+            </tr>
+            <tr>
+              <td><div class="date date-disabled">13</div></td>
+              <td><div class="date date-disabled">14</div></td>
+              <td><div class="date date-disabled">15</div></td>
+              <td><div class="date">16</div></td>
+              <td><div class="date">17</div></td>
+              <td><div class="date">18</div></td>
+              <td><div class="date">19</div></td>
+            </tr>
+            <tr>
+              <td><div class="date">20</div></td>
+              <td><div class="date">21</div></td>
+              <td><div class="date">22</div></td>
+              <td><div class="date">23</div></td>
+              <td><div class="date">24</div></td>
+              <td><div class="date">25</div></td>
+              <td><div class="date">26</div></td>
+            </tr>
+            <tr>
+              <td><div class="date">27</div></td>
+              <td><div class="date">28</div></td>
+              <td><div class="date">29</div></td>
+              <td><div class="date">30</div></td>
+              <td><div class="date">31</div></td>
+              <td><div class="date">&nbsp;</div></td>
+              <td><div class="date">&nbsp;</div></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="py-4">
+        <div class="font-semibold">January 2025</div>
+        <table class="search-dates-table">
+          <tbody>
+            <tr>
+              <td><div class="date">&nbsp;</div></td>
+              <td><div class="date">&nbsp;</div></td>
+              <td><div class="date date-disabled">1</div></td>
+              <td><div class="date date-disabled">2</div></td>
+              <td><div class="date date-disabled">3</div></td>
+              <td><div class="date date-disabled">4</div></td>
+              <td><div class="date date-disabled">5</div></td>
+            </tr>
+            <tr>
+              <td><div class="date date-disabled">6</div></td>
+              <td><div class="date date-disabled">7</div></td>
+              <td><div class="date date-disabled">8</div></td>
+              <td><div class="date date-disabled">9</div></td>
+              <td><div class="date date-disabled">10</div></td>
+              <td><div class="date date-disabled">11</div></td>
+              <td><div class="date date-disabled">12</div></td>
+            </tr>
+            <tr>
+              <td><div class="date date-disabled">13</div></td>
+              <td><div class="date date-disabled">14</div></td>
+              <td><div class="date date-disabled">15</div></td>
+              <td><div class="date">16</div></td>
+              <td><div class="date">17</div></td>
+              <td><div class="date">18</div></td>
+              <td><div class="date">19</div></td>
+            </tr>
+            <tr>
+              <td><div class="date">20</div></td>
+              <td><div class="date">21</div></td>
+              <td><div class="date">22</div></td>
+              <td><div class="date">23</div></td>
+              <td><div class="date">24</div></td>
+              <td><div class="date">25</div></td>
+              <td><div class="date">26</div></td>
+            </tr>
+            <tr>
+              <td><div class="date">27</div></td>
+              <td><div class="date">28</div></td>
+              <td><div class="date">29</div></td>
+              <td><div class="date">30</div></td>
+              <td><div class="date">31</div></td>
+              <td><div class="date">&nbsp;</div></td>
+              <td><div class="date">&nbsp;</div></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class="flex gap-2 pl-6 -mx-6 overflow-x-scroll flex-nowrap no-scrollbar"></div>
 </template>
+
+<style>
+.search-dates-table {
+  @apply border-separate;
+
+  td {
+    @apply relative box-border h-[43px] w-[45px] cursor-default border-0 bg-white p-0 text-center text-[rgb(34,34,34)];
+  }
+
+  .date {
+    @apply relative mx-1 flex flex-col items-center justify-center rounded-full border-[1.5px] border-solid border-white text-sm font-semibold text-[rgb(34,34,34)];
+  }
+
+  .date-disabled {
+    @apply text-[rgb(72,72,72)] no-underline opacity-25;
+  }
+}
+</style>
